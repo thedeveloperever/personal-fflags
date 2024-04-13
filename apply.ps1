8ca1848da6f2795be47bcf0b39b82795204de707
TheDeveloperEver <76601635+thedeveloperever@users.noreply.github.com>
2024-04-13 17:54:54 -0400

    Create apply.ps1

---
+++ apply.ps1
@@ -0,0 +1,8 @@
+$directory = get-childitem "$Env:LocalAppData\Roblox\Versions\version-*" -Name
+
+if(!(Test-Path "$directory\ClientSettings")) {
+  rd $directory\ClientSettings
+}
+
+md "$directory\ClientSettings"
+iwr https://raw.githubusercontent.com/thedeveloperever/personal-fflags/main/ClientAppSettings.json -o $directory\ClientSettings\ClientAppSettings.json
